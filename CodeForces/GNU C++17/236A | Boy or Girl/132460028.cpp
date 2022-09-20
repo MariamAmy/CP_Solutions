76fa786a45c1921da2fe249cfbe34cd898f5481f
Mariam <82214163+MariamAmy@users.noreply.github.com>
2022-09-20 12:28:33 +0200

    Committing files

---
+++ CodeForces/GNU C++17/236A | Boy or Girl/132460028.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+int main() {
+ios::sync_with_stdio(0);
+cin.tie(0);
+    string a;
+    vector<char> temp;
+    cin >> a;
+    int c=0;
+    for (int i = 0; i < a.size(); i++) {
+      if (find(temp.begin(), temp.end(), a[i]) == temp.end()){
+        for (int j=i+1; j< a.size(); j++){
+            if (a[i]==a[j]){
+                c++;
+                temp.push_back(a[i]);
+            }
+ 
+        }
+      }
+    }
+    if ((a.size()-c)%2!=0) cout<<"IGNORE HIM!"<<endl;
+    else cout<<"CHAT WITH HER!"<<endl;
+ 
+return 0;
+}
