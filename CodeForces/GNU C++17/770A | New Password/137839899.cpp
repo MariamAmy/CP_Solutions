6f96a62ef7877cf3664675add4db1d88f404374f
Mariam <82214163+MariamAmy@users.noreply.github.com>
2022-09-20 12:27:24 +0200

    Committing files

---
+++ CodeForces/GNU C++17/770A | New Password/137839899.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define lp(i, n) for(int i = 0; i < n; i++)
+ 
+int main(){
+    /*
+    int n, k, j = 0;
+    cin >> n >> k;
+    char c = 'a';
+    lp(i, n){
+        cout << c;
+        c++;
+        j++;
+        if(j == k){c = 'a'; j = 0;}
+    }
+    */
+    int  n, k;
+    cin >> n >>k;
+    char c = 'a';
+    lp(i, n){
+        cout << char (c + i % k);
+    }
+}
