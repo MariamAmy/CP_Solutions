ea2c69afc85fda91546177cef1eb9e73d49e5759
Mariam <82214163+MariamAmy@users.noreply.github.com>
2022-09-20 12:24:41 +0200

    Committing files

---
+++ CodeForces/GNU C++17/4A | Watermelon/155473188.cpp
@@ -0,0 +1,20 @@
+# include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+#define ll long long
+#define ln "\n"
+#define lp(i, n) for (int i = 0; i < n; i++)
+ 
+const int mod = (int)(1e9 + 7);
+const int mxN = 1005, oo = 0x3f3f3f3f;
+//vector <pair<int, int>> moves = {{-1,0}, {1,0}, {0,1}, {0, -1}, {1, -1}, {-1, 1}, {-1, -1},{1, 1}};
+ 
+ll sum=0;
+ 
+int main(){
+   std::ios_base::sync_with_stdio(false);
+   int n; cin >> n;
+   if(n&1 || n<=2) cout<<"NO"<<ln;
+   else cout<<"YES";
+}
