96c80c51295f923212a3bb8c3fe047feda8b8cee
Mariam <82214163+MariamAmy@users.noreply.github.com>
2022-09-20 12:28:29 +0200

    Committing files

---
+++ CodeForces/GNU C++17/381A | Sereja and Dima/132503862.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void print_arr(vector<int> arr){
+    for(int i = 0; i < arr.size(); i++){
+        cout << arr[i];
+    }
+}
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+    int x;
+    cin>>x;
+    int s=0, d=0;
+    vector<int> v(x);
+    for(int i=0; i< x; i++){
+        cin >> v[i];
+    }
+ 
+    for(int j=0; j< x; j++){
+        if(j % 2 == 0){
+            if(*v.begin() > *(v.end() - 1)){
+                    s+= *v.begin();
+                    v.erase(v.begin());
+            }else{
+                s+= *(v.end() - 1);
+                v.pop_back();
+            }
+        }else{
+            if(*v.begin() > *(v.end() - 1)){
+                    d+= *v.begin();
+                    v.erase(v.begin());
+            }else{
+                d+= *(v.end() - 1);
+                v.pop_back();
+            }
+        }
+    }
+    cout<< s << " " << d;
+}
